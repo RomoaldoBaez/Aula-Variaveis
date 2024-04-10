ea8c0ec19e812059f819f5a264a6a38080ea9632
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-10 11:53:44 -0300

    mudança toString

---
+++ index.js
@@ -0,0 +1,12 @@
+//Hello world
+console.log("Hello World!")
+//testenatdo a const
+const nome = "Romoaldo" 
+const idade = 19 
+console.log("meu nome é",nome,"e tenho",idade,"anos de idade ")
+//teste 
+const idadeNumero = 19
+const idadeTexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+
