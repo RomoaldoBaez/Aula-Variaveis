b488682c5a20ed33698822ee675d5c4dc5822ef7
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-11 11:31:42 -0300

    exercicios

--- template_javascript/template_javascript/index.js
+++ template_javascript/template_javascript/index.js
@@ -1,2 +1,16 @@
-let nome = prompt("Qual é seu nome?")
-alert(`seu nome é $(nome)` )
+//let nome = prompt("Qual é seu nome?")
+//alert(`seu nome é ${nome}` )
+
+//Exercícios de escrita de código 1 
+//let nome 
+//let idade  
+//console.log(typeof nome )
+//console.log(typeof idade ) 
+//undefined por nao ter nenhum valor atribuido 
+//let nome = prompt ("Qual é seu nome?")
+//alert(`seu nome é ${nome}`)
+//let idade = prompt ("qual sua idade")
+//alert(`sua idade é ${idade}`)
+
+//Exercício 2
+alert()
