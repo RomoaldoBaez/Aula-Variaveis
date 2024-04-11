4f7d3ffa25e58ecea612eaa7f2f2362b48b48480
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-11 10:16:34 -0300

    geral

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 //Hello world
-console.log("Hello World!")
+//console.log("Hello World!")
 //testenatdo a const
-const nome = "Romoaldo" 
-const idade = 19 
-console.log("meu nome é",nome,"e tenho",idade,"anos de idade ")
+//const nome = "Romoaldo" 
+//const idadeNumero = "19"
+//console.log("meu nome é",nome,"e tenho",idade,"anos de idade ")
 //teste 
-const idadeNumero = 19
-const idadeTexto = idadeNumero.toString()
-console.log(typeof idadeNumero)
-console.log(typeof idadeTexto)
-console.log("oi")
-
+//const idadeNumero = 19
+//const idadeTexto = idadeNumero.toString()
+//console.log(typeof idadeNumero)
+//console.log(typeof idadeTexto)
+//console.log("oi")
+//aula 2 Variaveis 
+//const primeiroNome = "Romoaldo"
+//const segundoNome = "Baez"
+//const idadeNumero = 19 
+//const serEstudante = "sim"
+//console.log("O seu nome é",primeiroNome,"o seu segundo nome é",segundoNome,"sua idade é",idadeNumero, "voçe é estudante",serEstudante,)
---
+++ template_javascript/template_javascript/index.js
@@ -0,0 +1,2 @@
+let nome = prompt("Qual é seu nome?")
+alert(`seu nome é $(nome)` )
