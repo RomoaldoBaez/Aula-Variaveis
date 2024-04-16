b5900bbba2db72791a02bf736d8fab79b0a40b23
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 10:19:12 -0300

    comparativos

--- AulaLogica/index.js
+++ AulaLogica/index.js
@@ -1,2 +1,23 @@
-let idade = 10
-alert(idade)
+//declaração de variaveis
+let primeiroNumero 
+let segundoNumero 
+let resultado
+let resultado2
+let resultado3
+let resultado4
+
+//leitura de dados informados pelo usuario
+primeiroNumero = prompt("Digite o primeiro numero")
+segundoNumero = prompt("Digite o segundo numero")
+
+//teste de commparação
+resultado = primeiroNumero === segundoNumero
+resultado2 = primeiroNumero < segundoNumero
+resultado3 = primeiroNumero > segundoNumero
+resultado4 = primeiroNumero !== segundoNumero
+
+//mostra o resultado 
+alert(resultado)
+alert(resultado2)
+alert(resultado3)
+alert(resultado4)
