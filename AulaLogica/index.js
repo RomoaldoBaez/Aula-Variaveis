1db3d33d6162e35826fa1fade1b5901803a882c1
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 11:33:09 -0300

    Exercício 5

--- AulaLogica/index.js
+++ AulaLogica/index.js
@@ -1,7 +1,9 @@
+
+/*
 //declaração de variaveis
 let primeiroNumero 
 let segundoNumero 
-let resultado
+let resultado1
 let resultado2
 let resultado3
 let resultado4
@@ -11,13 +13,72 @@ primeiroNumero = prompt("Digite o primeiro numero")
 segundoNumero = prompt("Digite o segundo numero")
 
 //teste de commparação
-resultado = primeiroNumero === segundoNumero
+resultado1 = primeiroNumero === segundoNumero
 resultado2 = primeiroNumero < segundoNumero
 resultado3 = primeiroNumero > segundoNumero
 resultado4 = primeiroNumero !== segundoNumero
 
 //mostra o resultado 
-alert(resultado)
+alert(resultado1)
 alert(resultado2)
 alert(resultado3)
 alert(resultado4)
+*/
+
+/*
+
+//Exercício de operadores logicos
+let a 
+let b 
+let c 
+let resultado1
+let resultado2
+let resultado3
+let resultado4
+
+
+//Leitura dos dados informados pelo usuario 
+a = true 
+b = false 
+c = true
+
+//realize a operação a && b 
+resultado1 = a && b
+resultado2 = b && c 
+resultado3 = a && c
+resultado4 = a && b && c
+alert(resultado1)
+alert(resultado2)
+alert(resultado3)
+alert(resultado4)
+
+*/
+
+//variaveis
+let nome 
+let idade 
+let anodeNascimento
+let anoAtual
+let maiorIdade
+let idade2050
+
+//valores informados
+nome = prompt("Digite seu nome")
+anodeNascimento = prompt("Qual seu ano de nascimento?")
+anoAtual = prompt("Qual seu ano atual?")
+
+
+//calculo dos valores
+
+idade  = anoAtual - anodeNascimento
+maiorIdade = idade >= 18 
+idade2050 = 2050 - anoAtual
+
+//resultados 
+alert(nome)
+alert(idade)
+alert(maiorIdade)
+alert(idade2050)
+
+
+//alert
