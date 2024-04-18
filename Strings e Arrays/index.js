305e9b1e3b518f50af642ae43c6041b3562a672d
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-18 11:55:57 -0300

    final da aula de strings e arrays

---
+++ Strings e Arrays/index.js
@@ -0,0 +1,39 @@
+
+//Concatenação
+//dados 
+/*nome = prompt("Digite seu nome")
+cor = prompt("Digite sua cor preferida ")
+alert(" A cor favorita de " + nome + " é " + cor + "!")
+//mensagem
+*/
+
+//Template strings
+/*
+nome = prompt("Digite seu nome")
+cor = prompt("Digite sua cor preferida")
+alert(` A cor preferida de ${nome} é ${cor} `)
+*/
+//Exercicio 2 toUpperCase replaceAll lenght 
+ 
+/*let frase = prompt("Escreva uma frase")
+
+console.log(frase .toUpperCase())
+console.log(frase .replaceAll("o","i"))
+console.log(frase .length)*/
+/*numero = prompt("Digite um numero de 0 a 4")
+const raças = ["Pastor-alemão" , "Buldogue" , "Golden retriever", "Beagle", "Poodle"]
+const raçaEscolhida = raças[numero]
+console.log(raçaEscolhida)*/
+
+const lista =[1, 2, 3, 4, 5, 6]
+lista .push(7)
+console.log(lista)
+lista .splice(3, 2)
+console.log(lista)
+console.log(lista .length)
+
+
+
+
+
+
