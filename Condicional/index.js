6adb5d6119d0d2d1faecb1bb45fe7c7520c00f0c
RomoaldoBaez <84526978+RomoaldoBaez@users.noreply.github.com>
2024-04-22 22:30:36 -0300

    Exercicio if

---
+++ Condicional/index.js
@@ -0,0 +1,16 @@
+//dados
+
+let num1 = prompt("Digite um numero")
+let num2 = prompt("Digite outro numero")
+
+condiçaoCorreta = num1 == num2 
+condiçaoCorreta = true
+condiçaoIncorreta = num1 != num2
+condiçaoIncorreta = false
+//if
+if (condiçaoCorreta){
+    console.log(condiçaoCorreta)
+} 
+if (condiçaoIncorreta){
+    console.log(condiçaoIncorreta)
+}
