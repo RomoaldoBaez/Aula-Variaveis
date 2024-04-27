d49840e78c54f4937434b79a2ddd3c08e26dc0b0
RomoaldoBaez <84526978+RomoaldoBaez@users.noreply.github.com>
2024-04-27 01:44:33 -0300

    objeto exeplo

---
+++ Objeto/index.js
@@ -0,0 +1,18 @@
+//Objeto exemplo
+let pessoa = {
+    nome: "jonatas",
+    idade: 35,
+    feliz: true,
+    pets:["cachorro", "gato"],
+    carros: {
+        camaro:{
+            placa: "738650",
+            cor: "verde"
+        },
+        uno: {
+            placa:"776658",
+            cor: "azul"
+        }
+    }
+}
+console.log(pessoa,idade)
